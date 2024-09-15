92c944f24c09cb1c0c988c0b16358c821346c9ce
Axel Hägglund <axel.hagglund@hotmail.com>
2024-09-15 21:39:47 +0200

    skapa ny tabell för 301player osv

---
+++ Models/301GamePlayer.cs
@@ -0,0 +1,9 @@
+﻿namespace DeathDartOfGlorySeasonOne.Models;
+
+public class _301GamePlayer
+{
+    public int Id { get; set; }
+    public int PlayerId { get; set; }
+    public Player Player { get; set; }
+    public int Score { get; set; }
+}
